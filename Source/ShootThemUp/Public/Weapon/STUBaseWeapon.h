3fdfac192517353ed78a2526a6b86944570b3e2a
arpharutyunyan <64978874+arpharutyunyan@users.noreply.github.com>
2021-12-09 23:29:38 +0400

    <Lesson 48>

--- Source/ShootThemUp/Public/Weapon/STUBaseWeapon.h
+++ Source/ShootThemUp/Public/Weapon/STUBaseWeapon.h
@@ -35,5 +35,10 @@ protected:
 
 	void MakeShot();
 
+	APlayerController* GetPlayerController() const;
+	bool GetPlayerViewPoint(FVector& ViewLocation, FRotator& ViewRotation) const;
+	FVector GetMuzzleWorldLocation() const;
+	bool GetTraceData(FVector& TraceStart, FVector& TraceEnd) const;
+	void MakeHit(FHitResult& HitResult, const FVector& TraceStart, const FVector& TraceEnd);
 
 };
