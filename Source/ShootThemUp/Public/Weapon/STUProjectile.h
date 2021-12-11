588f4591b7b1a55d336bd2974128d8b72b3ce0c5
arpharutyunyan <64978874+arpharutyunyan@users.noreply.github.com>
2021-12-11 23:46:33 +0400

    <Lesson 54>

---
+++ Source/ShootThemUp/Public/Weapon/STUProjectile.h
@@ -0,0 +1,27 @@
+// Shoot Them Up Game, All Rights Reserved
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "STUProjectile.generated.h"
+
+class USphereComponent;
+
+UCLASS()
+class SHOOTTHEMUP_API ASTUProjectile : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	
+	ASTUProjectile();
+
+protected:
+
+	UPROPERTY(VisibleDefaultsOnly, Category = "Components")
+		USphereComponent* CollisionComponent;
+	
+	virtual void BeginPlay() override;
+
+};
