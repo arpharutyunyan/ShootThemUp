2b0f893fbed7a6ee6ca682f73550312479a7d956
arpharutyunyan <64978874+arpharutyunyan@users.noreply.github.com>
2022-01-16 18:30:02 +0400

    <Lesson 135>

--- Source/ShootThemUp/Private/Menu/UI/STUMenuWidget.cpp
+++ Source/ShootThemUp/Private/Menu/UI/STUMenuWidget.cpp
@@ -6,6 +6,8 @@
 #include <Kismet/GameplayStatics.h>
 #include <STUGameInstance.h>
 #include <Kismet/KismetSystemLibrary.h>
+#include <Components/HorizontalBox.h>
+#include <Menu/UI/STULevelItemWidget.h>
 
 DEFINE_LOG_CATEGORY_STATIC(LogSTUGameInstance, All, All);
 
@@ -22,26 +24,74 @@ void USTUMenuWidget::NativeOnInitialized()
 	{
 		QuitGameButton->OnClicked.AddDynamic(this, &USTUMenuWidget::OnQuitGame);
 	}
+	InitLevelItems();
+}
 
+void USTUMenuWidget::InitLevelItems()
+{
+	const auto STUGameInstance = GetSTUGameInstance();
+	if (!STUGameInstance) return;
+
+	checkf(STUGameInstance->GetLevelsData().Num() != 0, TEXT("Levels data must not be empty!"));
+
+	if (!LevelItemBox) return;
+	LevelItemBox->ClearChildren();
+
+	for (auto LevelData : STUGameInstance->GetLevelsData())
+	{
+		const auto LevelItemWidget = CreateWidget<USTULevelItemWidget>(GetWorld(), LevelItemWidgetClass);
+		if (!LevelItemWidget) continue;
+
+		LevelItemWidget->SetLevelData(LevelData);
+		LevelItemWidget->OnLevelSelected.AddUObject(this, &USTUMenuWidget::OnLevelSelected);
+
+		LevelItemBox->AddChild(LevelItemWidget);
+		LevelItemWidgets.Add(LevelItemWidget);
+	}
+
+	if (STUGameInstance->GetStartupLevel().LevelName.IsNone())
+	{
+		OnLevelSelected(STUGameInstance->GetLevelsData()[0]);
+	}
+	else
+	{
+		OnLevelSelected(STUGameInstance->GetStartupLevel());
+	}
+}
+
+void USTUMenuWidget::OnLevelSelected(const FLevelData& Data)
+{
+	const auto STUGameInstance = GetWorld()->GetGameInstance<USTUGameInstance>();
+	if (!STUGameInstance) return;
+
+	STUGameInstance->SetStartupLevel(Data);
+
+	for (auto LevelItemWidget : LevelItemWidgets)
+	{
+		if (LevelItemWidget)
+		{
+			const auto IsSelected = Data.LevelName == LevelItemWidget->GetLevelData().LevelName;
+			LevelItemWidget->SetSelected(IsSelected);
+		}
+	}
 }
 
 void USTUMenuWidget::OnStartGame()
 {
-	if(!GetWorld()) return;
-
 	const auto STUGameInstance = GetWorld()->GetGameInstance<USTUGameInstance>();
 	if (!STUGameInstance) return;
 
-	if (STUGameInstance->GetStartupLevelName().IsNone())
-	{
-		UE_LOG(LogSTUGameInstance, Error, TEXT("Level name is None."));
-		return;
-	}
-
-	UGameplayStatics::OpenLevel(this, STUGameInstance->GetStartupLevelName());
+	UGameplayStatics::OpenLevel(this, STUGameInstance->GetStartupLevel().LevelName);
 }
 
 void USTUMenuWidget::OnQuitGame()
 {
 	UKismetSystemLibrary::QuitGame(this, GetOwningPlayer(), EQuitPreference::Quit, true);
+}
+
+USTUGameInstance* USTUMenuWidget::GetSTUGameInstance() const
+{
+	if (!GetWorld()) return nullptr;
+
+	return GetWorld()->GetGameInstance<USTUGameInstance>();
 }
--- Source/ShootThemUp/Public/Menu/UI/STUMenuWidget.h
+++ Source/ShootThemUp/Public/Menu/UI/STUMenuWidget.h
@@ -3,10 +3,15 @@
 #pragma once
 
 #include "CoreMinimal.h"
+#include <STUCoreTypes.h>
 #include "Blueprint/UserWidget.h"
 #include "STUMenuWidget.generated.h"
 
 class UButton;
+class UHorizontalBox;
+class USTULevelItemWidget;
+class USTUGameInstance;
+
 
 UCLASS()
 class SHOOTTHEMUP_API USTUMenuWidget : public UUserWidget
@@ -20,13 +25,27 @@ protected:
 	UPROPERTY(meta = (BindWidget))
 		UButton* QuitGameButton;
 
+	UPROPERTY(meta = (BindWidget))
+		UHorizontalBox* LevelItemBox;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, Category = "UI")
+		TSubclassOf<UUserWidget> LevelItemWidgetClass;
+
+
 	virtual void NativeOnInitialized() override;
 
 private:
+	UPROPERTY()
+		TArray<USTULevelItemWidget*> LevelItemWidgets;
+
 	UFUNCTION()
 		void OnStartGame();
 
 	UFUNCTION()
 		void OnQuitGame();
+
+	void InitLevelItems();
+	void OnLevelSelected(const FLevelData& Data);
+	USTUGameInstance* GetSTUGameInstance() const;
 	
 };
